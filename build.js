9aaf950effd1028464d72c847073eef7040e1a0e
José Pablo <65085262+astrovini@users.noreply.github.com>
2025-11-07 00:42:05 -0600

    Add files via upload

--- build.js
+++ build.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 require('dotenv').config();
 
+// Create public directory if it doesn't exist
+if (!fs.existsSync('public')) {
+  fs.mkdirSync('public');
+}
+
 // Read the template file
 const template = fs.readFileSync('src/fitbot-app.html', 'utf8');
 
@@ -9,10 +14,8 @@ const output = template
   .replace('__SUPABASE_URL__', process.env.VITE_SUPABASE_URL)
   .replace('__SUPABASE_ANON_KEY__', process.env.VITE_SUPABASE_ANON_KEY);
 
-// Write the output file
-fs.writeFileSync('fitbot-app.html', output);
-
-// Copy index.html to root
-fs.copyFileSync('src/index.html', 'index.html');
+// Write the output files to public directory
+fs.writeFileSync('public/fitbot-app.html', output);
+fs.copyFileSync('src/index.html', 'public/index.html');
 
 console.log('Build complete!');
